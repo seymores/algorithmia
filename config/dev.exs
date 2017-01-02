edf12a3a03cc29846f18f17b1cdd25b039057fe6
Ping <seymores@gmail.com>
2017-01-02 22:17:01 +0800

    First commit

---
+++ config/dev.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :algorithmia,
+  simple_api_key: "simlvC7/1t3U1fwSuZjZDjxl0ts1"
---
+++ test/algorithmia_test.exs
@@ -0,0 +1,8 @@
+defmodule AlgorithmiaTest do
+  use ExUnit.Case
+  doctest Algorithmia
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
